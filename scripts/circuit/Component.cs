7abb269f8a9912bf25601d3bcd3bbb30f76a382b
jdugan0 <justin.d.dugan@gmail.com>
2025-10-02 13:58:31 -0400

    power fix

--- scripts/circuit/Component.cs
+++ scripts/circuit/Component.cs
@@ -6,12 +6,13 @@ public partial class Component : Node
     [Export] public Pin[] pins { get; private set; }
     [Export] public ComponentProperty componentProperty;
     [Export] public double maxEnergy = 10;
+    [Export] public double wattage = 5;
     double energy;
     public double solvedCurrent;
     public double solvedVoltage;
     public override void _Process(double delta)
     {
-        energy += solvedCurrent * solvedCurrent * solvedCurrent * delta;
+        energy += (solvedCurrent * solvedCurrent * solvedVoltage - wattage) * delta;
         if (energy >= maxEnergy)
         {
             PlacementManager.instance.PowerBlow(this);
